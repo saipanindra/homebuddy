ec30f7fef0a0e038892a6f3b0f4958b21deb4eec
saipanindra <saipanindra9@gmail.com>
2013-07-17 17:06:59 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homebuddy::Application.config.secret_token = '02d06540dd93ff02f115946e50b69b31ac347e8b2b01e9b003b72d0e72f4aaf535d209d9dd77a3478da01c490e01a557ef8350dd1dad1e34464c65b0389b646e'
