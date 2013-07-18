1fcd92250dc8c8d827f4dd0c2d48f201c6def689
saipanindra <saipanindra9@gmail.com>
2013-07-17 22:25:21 -0400

    Make the Work In Progress Page the Home page

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Homebuddy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'static_pages#wip'
 
   # See how all your routes lay out with "rake routes"
 
